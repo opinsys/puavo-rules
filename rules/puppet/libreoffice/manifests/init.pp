1725e6765e9b007655f2747e06ac23e34d5620b2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-08 10:30:18 +0200

    The proper value for SAL_USE_VCLPLUGIN (for Libreoffice 5) is "kde4". Also depend properly on the "libreoffice-kde"-package.

--- rules/puppet/libreoffice/manifests/init.pp
+++ rules/puppet/libreoffice/manifests/init.pp
@@ -1,11 +1,15 @@
 class libreoffice {
   include libreoffice::config,
-	  libreoffice::default_formats
+	  libreoffice::default_formats,
+	  packages
 
   # Use the KDE-interface by default, because Libreoffice 5 reveals
   # an annoying bug in the GTK-libraries in Ubuntu Trusty.
   file {
     '/etc/X11/Xsession.d/42puavo-setup-libreoffice':
-      content => "SAL_USE_VCLPLUGIN=kde\nexport SAL_USE_VCLPLUGIN\n";
+      content => "SAL_USE_VCLPLUGIN=kde4\nexport SAL_USE_VCLPLUGIN\n",
+      require => Package['libreoffice-kde'];
   }
+
+  Package <| title == libreoffice-kde |>
 }
