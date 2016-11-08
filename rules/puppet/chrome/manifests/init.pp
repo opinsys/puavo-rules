ba6bf74c4b0284ad0fa993aae38ce3f4cd8497a2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-08 14:40:44 +0200

    Download system-wide amd64-version of Adobe Flashplugin (PPAPI) for Google Chrome, and use it if it exists.

--- rules/puppet/chrome/manifests/init.pp
+++ rules/puppet/chrome/manifests/init.pp
@@ -8,6 +8,12 @@ class chrome {
       ;
   }
 
+  exec {
+    '/usr/local/sbin/puavo-get-pepperflash-amd64':
+      creates => '/usr/lib/adobe-flashplugin/libpepflashplayer-amd64.so',
+      require => File['/usr/local/sbin/puavo-get-pepperflash-amd64'];
+  }
+
   file {
     '/etc/apt/sources.list.d/google-chrome.list':
       require => Package['google-chrome-stable:amd64'],
@@ -15,8 +21,13 @@ class chrome {
 
     '/usr/bin/google-chrome-stable':
       mode    => 755,
-      require => Dpkg::Simpledivert['/usr/bin/google-chrome-stable'],
+      require => [ Dpkg::Simpledivert['/usr/bin/google-chrome-stable']
+		 , Exec['/usr/local/sbin/puavo-get-pepperflash-amd64'] ],
       source  => 'puppet:///modules/chrome/google-chrome-stable';
+
+    '/usr/local/sbin/puavo-get-pepperflash-amd64':
+      mode   => 755,
+      source => 'puppet:///modules/chrome/puavo-get-pepperflash-amd64';
 
     '/usr/share/applications/google-chrome.desktop':
       require => Dpkg::Simpledivert['/usr/share/applications/google-chrome.desktop'],
