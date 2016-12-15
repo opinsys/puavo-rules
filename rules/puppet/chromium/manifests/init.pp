96ced8fc75ed82ab0e3d10dc2d2d7ae85fb05742
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-12-15 16:57:19 +0200

    Refactor chromium/office365_tweaks a bit.

--- rules/puppet/chromium/manifests/init.pp
+++ rules/puppet/chromium/manifests/init.pp
@@ -3,9 +3,32 @@ class chromium {
           dpkg,
           packages
 
+  define install_policy () {
+    $policy_name = $title
+
+    file {
+      "/etc/chromium/policies/managed/${policy_name}.json":
+	content => template("chromium/${policy_name}.json");
+
+      "/etc/opt/chrome/policies/managed/${policy_name}.json":
+	ensure  => link,
+	require => File["/etc/chromium/policies/managed/${policy_name}.json"],
+	target  => "/etc/chromium/policies/managed/${policy_name}.json";
+    }
+  }
+
   dpkg::simpledivert { '/usr/bin/chromium-browser': ; }
 
   file {
+    [ '/etc/chromium'
+    , '/etc/chromium/policies'
+    , '/etc/chromium/policies/managed'
+    , '/etc/opt'
+    , '/etc/opt/chrome'
+    , '/etc/opt/chrome/policies'
+    , '/etc/opt/chrome/policies/managed' ]:
+      ensure => directory;
+
     '/usr/bin/chromium-browser':
       mode   => 755,
       source => 'puppet:///modules/chromium/chromium-browser';
