30bd89e2e0a3f1c590c21872fe4801e31a6083da
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-02-16 14:35:19 +0200

    Install all libreoffice 5.3 packages (with language support for those we need).

--- rules/puppet/packages/manifests/languages/fi.pp
+++ rules/puppet/packages/manifests/languages/fi.pp
@@ -7,8 +7,14 @@ class packages::languages::fi {
     , 'kde-l10n-fi'
     , 'language-pack-fi'
     , 'language-pack-kde-fi'
+    , 'libobasis5.3-fi'
+    , 'libobasis5.3-fi-base'
+    , 'libobasis5.3-fi-calc'
+    , 'libobasis5.3-filter-data'
+    , 'libobasis5.3-fi-math'
+    , 'libobasis5.3-fi-res'
+    , 'libobasis5.3-fi-writer'
     , 'libreoffice5.3-fi'
-    , 'libreoffice-voikko'
     , 'thunderbird-locale-fi' ]:
       tag => [ 'language-fi', 'ubuntu', ];
   }
