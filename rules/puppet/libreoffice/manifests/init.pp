b57f0ac18187ad255ac97000f93c574b624da680
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-02-23 10:54:25 +0200

    Install libreoffice 5.2 packages, also change the default applications to match.

--- rules/puppet/libreoffice/manifests/init.pp
+++ rules/puppet/libreoffice/manifests/init.pp
@@ -6,8 +6,8 @@ class libreoffice {
     '/etc/puavo-external-files-actions.d/libreoffice':
       content => template('libreoffice/puavo-external-files-actions.d/libreoffice'),
       mode    => 755,
-      require => Package['libreoffice5.3-writer'];
+      require => Package['libreoffice5.2-writer'];
   }
 
-  Package <| tag == "libreoffice5.3-writer" |>
+  Package <| tag == "libreoffice5.2-writer" |>
 }
--- rules/puppet/libreoffice/manifests/voikko.pp
+++ rules/puppet/libreoffice/manifests/voikko.pp
@@ -3,12 +3,12 @@ class libreoffice::voikko {
   include packages::languages::fi
 
   file {
-    '/opt/libreoffice5.3/share/extensions/voikko': 
+    '/opt/libreoffice5.2/share/extensions/voikko': 
       ensure  => link,
-      require => [ Package['libreoffice5.3'], Package['libreoffice-voikko'] ],
+      require => [ Package['libreoffice5.2'], Package['libreoffice-voikko'] ],
       target  => '/usr/lib/libreoffice/share/extensions/voikko';
   }
 
-  Package <| title == "libreoffice5.3"
+  Package <| title == "libreoffice5.2"
           or title == libreoffice-voikko |>
 }
