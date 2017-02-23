64ca24dbc572657fcf990ed0862e1a633d200ad8
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-02-23 11:36:14 +0200

    Language packages for Libreoffice should also be of version 5.2.

--- rules/puppet/packages/manifests/languages/fi.pp
+++ rules/puppet/packages/manifests/languages/fi.pp
@@ -7,14 +7,14 @@ class packages::languages::fi {
     , 'kde-l10n-fi'
     , 'language-pack-fi'
     , 'language-pack-kde-fi'
-    , 'libobasis5.3-fi'
-    , 'libobasis5.3-fi-base'
-    , 'libobasis5.3-fi-calc'
-    , 'libobasis5.3-filter-data'
-    , 'libobasis5.3-fi-math'
-    , 'libobasis5.3-fi-res'
-    , 'libobasis5.3-fi-writer'
-    , 'libreoffice5.3-fi'
+    , 'libobasis5.2-fi'
+    , 'libobasis5.2-fi-base'
+    , 'libobasis5.2-fi-calc'
+    , 'libobasis5.2-filter-data'
+    , 'libobasis5.2-fi-math'
+    , 'libobasis5.2-fi-res'
+    , 'libobasis5.2-fi-writer'
+    , 'libreoffice5.2-fi'
     , 'thunderbird-locale-fi' ]:
       tag => [ 'language-fi', 'ubuntu', ];
   }
