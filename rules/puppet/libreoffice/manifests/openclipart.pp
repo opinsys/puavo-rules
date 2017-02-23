ac7f41d191063476820bac256c36f439058aca41
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-02-23 11:36:48 +0200

    Linking to libreoffice clipart gallery did not work, instead, merge the two galleries.

--- rules/puppet/libreoffice/manifests/openclipart.pp
+++ rules/puppet/libreoffice/manifests/openclipart.pp
@@ -3,10 +3,10 @@ class libreoffice::openclipart {
 
   file {
     '/opt/libreoffice5.2/share/gallery': 
-      ensure  => link,
       require => [ Package['libreoffice5.2']
 		 , Package['openclipart-libreoffice'] ],
-      target  => '/usr/lib/libreoffice/share/extensions/gallery';
+      recurse => true,
+      source  => '/usr/lib/libreoffice/share/gallery';
   }
 
   Package <| title == "libreoffice5.2"
