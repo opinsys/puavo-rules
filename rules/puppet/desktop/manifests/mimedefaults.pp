6ce87a715b8502636b0f31a55858b7f11103320e
Antti Sokero <antti.sokero@opinsys.fi>
2017-04-19 12:55:11 +0300

    Add mimetypes for racket files to be opened with drracket.

--- rules/puppet/desktop/manifests/mimedefaults.pp
+++ rules/puppet/desktop/manifests/mimedefaults.pp
@@ -1,6 +1,12 @@
 class desktop::mimedefaults {
   require packages
 
+  exec {
+    'desktop-update-mime-database':
+      command     => '/usr/bin/update-mime-database /usr/share/mime',
+      refreshonly => true;
+  }
+
   file {
     '/usr/share/applications/defaults.list':
        content => template('desktop/defaults.list');
@@ -9,6 +15,12 @@ class desktop::mimedefaults {
   file {
     '/etc/gnome/defaults.list':
        content => template('desktop/defaults.list');
+  }
+
+  file {
+    '/usr/share/mime/packages/drracket.xml':
+       content => 'puppet:///modules/desktop/drracket.xml';
+       notify  => Exec['desktop-update-mime-database'],
   }
 
   Package <| title == desktop-file-utils |>
