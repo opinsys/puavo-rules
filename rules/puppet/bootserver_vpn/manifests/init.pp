1e609377cbd1f289e6ecb2fdca9718645effd078
Christoph Joohs <christoph.joohs@opinsys.fi>
2018-10-04 13:14:54 +0300

    Remove /etc/openvpn/altvpn1.conf on bootservers and stop the corresponding openvpn process.

--- rules/puppet/bootserver_vpn/manifests/init.pp
+++ rules/puppet/bootserver_vpn/manifests/init.pp
@@ -1,6 +1,11 @@
 class bootserver_vpn {
   include bootserver_config
 
+ file {
+    '/etc/openvpn/altvpn1.conf':
+      ensure  => absent,
+      notify  => Exec['altvpn1'];
+
   file {
     '/etc/openvpn/puavo.conf':
       content => template('bootserver_vpn/puavo.conf'),
@@ -16,5 +21,11 @@ class bootserver_vpn {
     'openvpn':
       enable => true,
       ensure => running;
+  }
+
+  exec {
+    'altvpn1':
+      command     => '/usr/sbin/service openvpn stop altvpn1',
+      refreshonly => true;
   }
 }
